65e52bc3c5d592bb33517057d058400cc84c3317
Paul_Luque <pluquec@unsa.edu.pe>
2024-05-22 09:38:58 -0500

    descifrado cesar

--- cifradoCesar.py
+++ cifradoCesar.py
@@ -48,24 +48,46 @@ def interfaz_usuario_cifrado():
 
 
 # 2 Uso del algoritmo -----------------------
-
+# COMPLETADO EN EL DOC
 
 # 4 Verificar el cypher
-
+# COMPLETADO EN EL DOC
 
 # 5 Algoritmo de descrifrado
+def descifrar_texto(texto_cifrado, desplazamiento):
+    texto_descifrado = ''
+    for caracter in texto_cifrado:
+        if caracter in ALFABETO:
+            indice_cifrado = ALFABETO.index(caracter)
+            indice_original = (indice_cifrado - desplazamiento) % 27
+            texto_descifrado += ALFABETO[indice_original]
+        else:
+            texto_descifrado += caracter  # Para caracteres no incluidos en el alfabeto
+    return texto_descifrado
 
 
+def interfaz_usuario_descifrado():
+    texto_cifrado = input("Ingrese el texto cifrado: ").strip()
+    desplazamiento = int(input("Desplazamiento: "))
+        
+    texto_resultante = descifrar_texto(texto_cifrado, desplazamiento)
+    print("Texto descifrado:", texto_resultante)
+    
 ## app
 
     
 if __name__ == "__main__":
     menu = {
         "1" : "Cifrado cesar",
+        "2" : "Descifrado cesar"
     }
     
     for key, val in menu.items():
         print(f"Opcion {key}: {val}")
     
-    if(input() == "1"):
+    opcion = input("Opcion: ")
+    if(opcion == "1"):
         interfaz_usuario_cifrado()
+    
+    if(opcion == "2"):
+        interfaz_usuario_descifrado()
